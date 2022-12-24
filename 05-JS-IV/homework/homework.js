bd703e84a887aa811b74a3dc39e3e18707c787f0
benjagatti <benjagatti13579@gmail.com>
2022-12-24 13:35:29 -0300

    homeworke 4 finished final version2

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -153,7 +153,7 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
   producto.calcularPrecioDescuento = function () {
-     return producto.precio - (producto.precio * producto.porcentajeDeDescuento);
+     return producto.precio - (producto.precio * producto.porcentajeDeDescuento)
   } 
   return producto;
 }
